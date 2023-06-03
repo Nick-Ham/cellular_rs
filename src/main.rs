9a386ebf1527c9944fcb97113d70fbfc62ac3a19
Nick-Ham <nick.mart3.14@gmail.com>
2023-06-03 00:59:20 -0500

    finished mvp simulation code

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,34 @@
 use glam::IVec2;
 use raylib::prelude::*;
-use std::time;
 
-//this is a comment
+const WINDOW_WIDTH: i32 = 500;
+const WINDOW_HEIGHT: i32 = 500;
 
-const WINDOW_WIDTH: i32 = 1820; //640
-const WINDOW_HEIGHT: i32 = 980; //480
-
-const CELL_SIZE: i32 = 60;
+const CELL_SIZE: i32 = 30;
 const CELL_ON_COLOR: Color = Color::WHITE;
 const CELL_OFF_COLOR: Color = Color::BLACK;
-const CELL_PADDING: i32 = 6;
+const CELL_PADDING: i32 = 2;
 
 const BACKGROUND_COLOR: Color = Color::GRAY;
+
+const FRAMES_PER_SECOND: f32 = 2.0;
+const FRAME_TIME_MILLIS: f32 = 1.0 / FRAMES_PER_SECOND;
 
 macro_rules! make_blinker {
     ($grid:expr, $position:expr) => {
         for i in -1..=1 {
             $grid[($position.x) as usize][($position.y + i) as usize].state = true;
         }
+    };
+}
+
+macro_rules! make_glider {
+    ($grid:expr, $position:expr) => {
+        $grid[($position.x) as usize][($position.y) as usize].state = true;
+        $grid[($position.x + 1) as usize][($position.y + 1) as usize].state = true;
+        $grid[($position.x + 1) as usize][($position.y + 2) as usize].state = true;
+        $grid[($position.x) as usize][($position.y + 2) as usize].state = true;
+        $grid[($position.x + -1) as usize][($position.y + 2) as usize].state = true;
     };
 }
 
@@ -63,27 +73,92 @@ fn main() {
 
     fill_cells_grid(&mut grid);
 
-    make_blinker!(grid, IVec2::new(3, 3));
+    make_glider!(grid, IVec2::new(1, 0));
 
-    let world_start = time::Instant::now();
+    let mut elapsed_time = 0.0;
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(BACKGROUND_COLOR);
 
+        while elapsed_time > FRAME_TIME_MILLIS {
+            update_cells(&mut grid);
+            elapsed_time -= FRAME_TIME_MILLIS;
+        }
+
         draw_cells(&grid, &mut d);
+        elapsed_time += d.get_frame_time();
     }
 }
 
-fn _update_cells(cells: &mut Vec<Vec<Cell>>) {
-    let deltas: Vec<Vec<bool>> = _get_cell_deltas(cells);
+fn update_cells(cells: &mut Vec<Vec<Cell>>) {
+    let new_states: Vec<Vec<bool>> = get_grid_next_state(cells);
+    for x in 0..get_grid_size().x {
+        for y in 0..get_grid_size().y {
+            cells[x as usize][y as usize].state = new_states[x as usize][y as usize];
+        }
+    }
 }
 
-fn _get_cell_deltas(cells: &Vec<Vec<Cell>>) -> Vec<Vec<bool>> {
-    //for x in 0..get_grid_size().x
+fn get_grid_next_state(cells: &Vec<Vec<Cell>>) -> Vec<Vec<bool>> {
+    let mut states: Vec<Vec<bool>> = Vec::new();
+    for _ in 0..get_grid_size().x {
+        let column: Vec<bool> = Vec::new();
+        states.push(column);
+    }
+
+    for x in 0..get_grid_size().x {
+        for y in 0..get_grid_size().y {
+            states[x as usize].push(get_cell_next_state(cells, IVec2 { x, y }));
+        }
+    }
+
+    states
 }
 
-fn _get_cell_delta(neighbors: &Vec<Cell>, cell: &Cell) -> bool {
-    false
+fn get_cell_next_state(cells: &Vec<Vec<Cell>>, cell_index: IVec2) -> bool {
+    let num_living = get_num_of_living_neighbors(cells, cell_index);
+    if cells[cell_index.x as usize][cell_index.y as usize].state == true {
+        match num_living {
+            2 | 3 => true,
+            _ => false,
+        }
+    } else {
+        match num_living {
+            3 => true,
+            _ => false,
+        }
+    }
+}
+
+fn get_num_of_living_neighbors(cells: &Vec<Vec<Cell>>, cell_index: IVec2) -> i32 {
+    let mut living: i32 = 0;
+
+    for i in -1..=1 {
+        for j in -1..=1 {
+            if i == 0 && j == 0 {
+                continue;
+            }
+
+            let next_x: i32 = cell_index.x + i;
+            let next_y: i32 = cell_index.y + j;
+
+            if next_x < 0 || next_y < 0 {
+                continue;
+            }
+
+            if next_x >= get_grid_size().x || next_y >= get_grid_size().y {
+                continue;
+            }
+
+            if cells[next_x as usize][next_y as usize].state == false {
+                continue;
+            }
+
+            living += 1;
+        }
+    }
+
+    living
 }
 
 fn draw_cells(cells: &Vec<Vec<Cell>>, draw_handle: &mut RaylibDrawHandle) {
